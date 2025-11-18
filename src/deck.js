b0335cd66694f1c15fd0f23188e0e738b2a487f8
hassanalisarah236 <hassanalisarah@gmail.com>
2025-11-18 21:06:41 +0000

    initial commit message

--- src/deck.js
+++ src/deck.js
@@ -1,5 +1,27 @@
-import './App.css';
+const SUITS = ["♠","♥", "♣", "♦"]
+const VALUES = [ "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+]
 
+export default Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards
+  }
+}
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
+}
+
+function freshDeck(){
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
+    })
+  })
+}
 function App() {
   return (
     <div className="App">
